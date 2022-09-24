22e3b4f37c095b67d107e1cf88257475e4a3c4c2
Robsonvieira26 <robson.vieira@ufu.br>
2022-09-24 18:39:33 -0300

    REFACTOR:Organização de componentes essenciais

--- src/components/essential/AppMenu.vue
+++ src/components/essential/AppMenu.vue
@@ -1,6 +1,7 @@
 <template>
 	<div class="layout-menu-container">
-		<AppSubmenu :items="model" class="layout-menu" :root="true" @menuitem-click="onMenuItemClick" @keydown="onKeyDown" />
+		<AppSubmenu :items="model" class="layout-menu" :root="true" @menuitem-click="onMenuItemClick"
+			@keydown="onKeyDown" />
 		<a href="https://www.primefaces.org/primeblocks-vue" class="block mt-3">
 			<img alt="primeblocks" :src="bannerImage()" class="w-full" />
 		</a>
@@ -14,10 +15,10 @@ export default {
 	props: {
 		model: Array
 	},
-    methods: {
-        onMenuItemClick(event) {
-            this.$emit('menuitem-click', event);
-        },
+	methods: {
+		onMenuItemClick(event) {
+			this.$emit('menuitem-click', event);
+		},
 		onKeyDown(event) {
 			const nodeElement = event.target;
 			if (event.code === 'Enter' || event.code === 'Space') {
@@ -28,7 +29,7 @@ export default {
 		bannerImage() {
 			return this.$appState.darkTheme ? 'images/banner-primeblocks-dark.png' : 'images/banner-primeblocks.png';
 		}
-    },
+	},
 	computed: {
 		darkTheme() {
 			return this.$appState.darkTheme;
--- src/components/essential/AppTopbar.vue
+++ src/components/essential/AppTopbar.vue
@@ -1,12 +1,12 @@
 <template>
 	<div class="layout-topbar">
-		<router-link to="/" class="layout-topbar-logo">
-			<img alt="Logo" :src="topbarImage()" />
-			<span>SAKAI</span>
-		</router-link>
 		<button class="p-link layout-menu-button layout-topbar-button" @click="onMenuToggle">
 			<i class="pi pi-bars"></i>
 		</button>
+		<router-link to="/" class="layout-topbar-logo">
+			<!-- <img alt="Logo" :src="topbarImage()" /> -->
+			<span>Sistema Apo</span>
+		</router-link>
 
 		<button class="p-link layout-topbar-menu-button layout-topbar-button"
 			v-styleclass="{ selector: '@next', enterClass: 'hidden', enterActiveClass: 'scalein', 
@@ -14,18 +14,7 @@
 			<i class="pi pi-ellipsis-v"></i>
 		</button>
 		<ul class="layout-topbar-menu hidden lg:flex origin-top">
-			<li>
-				<button class="p-link layout-topbar-button">
-					<i class="pi pi-calendar"></i>
-					<span>Events</span>
-				</button>
-			</li>
-			<li>
-				<button class="p-link layout-topbar-button">
-					<i class="pi pi-cog"></i>
-					<span>Settings</span>
-				</button>
-			</li>
+			
 			<li>
 				<button class="p-link layout-topbar-button">
 					<i class="pi pi-user"></i>
