0d597de2e3c21e590bc399a4897e6a923cf684af
Robsonvieira26 <robson.vieira@ufu.br>
2022-10-15 13:35:19 -0300

    CHORE<QuestionChoices>: Resposta em texto livre

--- src/components/crud-components/QuestionChoice.vue
+++ src/components/crud-components/QuestionChoice.vue
@@ -6,17 +6,22 @@
     <div v-if="qType == 'Uniquecheckbox'">
       <Uniquecheckbox :values="qValues" :type="qType" />
     </div>
+    <div v-if="qType == 'Text'">
+      <TextQuestion :values="qValues" :type="qType" />
+    </div>
   </div>
 </template>
 <script>
 import Multiplecheckbox from "@/components/crud-components/question-types/Multiplecheckbox.vue";
 import Uniquecheckbox from "@/components/crud-components/question-types/Uniquecheckbox.vue";
+import TextQuestion from "@/components/crud-components/question-types/TextQuestion.vue";
 
 export default {
   name: "QuestionChoice",
   components: {
     Multiplecheckbox,
     Uniquecheckbox,
+    TextQuestion,
   },
   data() {
     return {
---
+++ src/components/crud-components/question-types/TextQuestion.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="p-3">
+    <div class="py-2">
+      <div class="py-2" v-for="(valueQuestion, index) in values" :key="index">
+        <div class="field-checkbox mb-0">
+          <Textarea v-model="Texto" :autoResize="true" rows="5" cols="80" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  // name: "MultipleCheckbox",
+  data() {
+    return {
+      Texto: "",
+    };
+  },
+  props: {
+    type: {
+      type: String,
+      default: "Multiplecheckbox",
+      required: true,
+    },
+    values: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
