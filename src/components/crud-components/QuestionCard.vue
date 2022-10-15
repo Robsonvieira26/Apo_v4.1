eebe2d945f0a4c1ec484fb8f3635a2b26e8fe6e5
Robsonvieira26 <robson.vieira@ufu.br>
2022-10-15 14:55:22 -0300

    CHORE<QuestionChoice>:Botão de editar pergunta

--- src/components/crud-components/QuestionCard.vue
+++ src/components/crud-components/QuestionCard.vue
@@ -1,6 +1,29 @@
 <template>
-  <div class="card border-primary border-round surface-overlay border-1 border-round">
-    <QuestionTittle :qTittle="qTittle" />
+  <div
+    class="card border-primary border-round surface-overlay border-1 border-round"
+  >
+    <div class="text-900 py-2 text-3xl font-medium mb-3 align-items-center">
+      <div class="flex justify-content-between flex-wrap">
+        <div
+          class="flex align-items-center justify-content-center font-bold text-white"
+        >
+          <QuestionTittle :qTittle="qTittle" />
+        </div>
+        <div
+          class="flex align-items-center justify-content-center font-bold text-white"
+        >
+          <SplitButton
+            label="Editar"
+            icon="pi pi-pencil"
+            class="p-button-text"
+            v-on:click="editarPergunta()"
+            :model="items"
+          ></SplitButton>
+        </div>
+      </div>
+    </div>
+    <Divider />
+
     <QuestionChoice :qType="qType" :qValues="qValues" />
   </div>
 </template>
@@ -27,6 +50,24 @@ export default {
       type: String,
       required: true,
     },
+  },
+  data() {
+    return {
+      items: [
+        {
+          label: "Remover",
+          icon: "pi pi-times",
+          command: () => {
+            this.$toast.add({
+              severity: "warn",
+              summary: "Delete",
+              detail: "Data Deleted",
+              life: 3000,
+            });
+          },
+        },
+      ],
+    };
   },
 };
 </script>
