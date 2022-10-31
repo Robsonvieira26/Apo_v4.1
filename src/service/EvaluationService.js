fac0009b26c8e11c988e677246c408998ad4d64c
Robsonvieira26 <robson.vieira@ufu.br>
2022-10-31 12:44:34 -0300

    REFACTOR<ServiceEvaluation>

--- src/service/EvaluationService.js
+++ src/service/EvaluationService.js
@@ -2,7 +2,7 @@ export default class EvaluationService {
 
 
 getQuestions() {
-  return fetch('data/evaluationTest.json').then(res => res.json()).then(d => d.data);
+  return fetch('data/evaluation.json').then(res => res.json()).then(d => d.data);
   }
 
 
