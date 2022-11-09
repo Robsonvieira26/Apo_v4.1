09ea1d31c7627bca5640e832713365581658f243
Robsonvieira26 <robson.vieira@ufu.br>
2022-11-09 18:48:15 -0300

    CLEAN: Limpando arquivos inuteis

--- src/views/evaluation.vue
+++ src/views/evaluation.vue
@@ -131,8 +131,6 @@
 
 <script>
 
-import { FilterMatchMode } from 'primevue/api';
-import ProductService from '../service/ProductService';
 
 export default {
   data() {
@@ -149,21 +147,11 @@ export default {
         { name: "Avaliação de teste 1", id: 1 },
         { name: "Avaliação de teste 2", id: 2 },
         { name: "Avaliação de teste 3", id: 3 }],
-      statuses: [
-        { label: 'INSTOCK', value: 'instock' },
-        { label: 'LOWSTOCK', value: 'lowstock' },
-        { label: 'OUTOFSTOCK', value: 'outofstock' }
-      ]
+
     }
   },
-  productService: null,
-  created() {
-    this.productService = new ProductService();
-    this.initFilters();
-  },
-  mounted() {
-    this.productService.getProducts().then(data => this.products = data);
-  },
+
+
   methods: {
     dialogMessage(text, id, buttonID) {
       if (buttonID == 1) {
@@ -173,89 +161,11 @@ export default {
 
     },
 
-    formatCurrency(value) {
-      if (value)
-        return value.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
-      return;
-    },
-    openNew() {
-      this.product = {};
-      this.submitted = false;
-      this.productDialog = true;
-    },
+    
     hideDialog() {
       this.productDialog = false;
       this.submitted = false;
     },
-    saveProduct() {
-      this.submitted = true;
-      if (this.product.name.trim()) {
-        if (this.product.id) {
-          this.product.inventoryStatus = this.product.inventoryStatus.value ? this.product.inventoryStatus.value : this.product.inventoryStatus;
-          this.products[this.findIndexById(this.product.id)] = this.product;
-          this.$toast.add({ severity: 'success', summary: 'Successful', detail: 'Product Updated', life: 3000 });
-        }
-        else {
-          this.product.id = this.createId();
-          this.product.code = this.createId();
-          this.product.image = 'product-placeholder.svg';
-          this.product.inventoryStatus = this.product.inventoryStatus ? this.product.inventoryStatus.value : 'INSTOCK';
-          this.products.push(this.product);
-          this.$toast.add({ severity: 'success', summary: 'Successful', detail: 'Product Created', life: 3000 });
-        }
-        this.productDialog = false;
-        this.product = {};
-      }
-    },
-    editProduct(product) {
-      this.product = { ...product };
-      this.productDialog = true;
-    },
-    confirmDeleteProduct(product) {
-      console.log("Excluindo o produto");
-      console.log(product);
-    },
-    deleteProduct() {
-      this.products = this.products.filter(val => val.id !== this.product.id);
-      this.deleteProductDialog = false;
-      this.product = {};
-      this.$toast.add({ severity: 'success', summary: 'Successful', detail: 'Product Deleted', life: 3000 });
-    },
-    findIndexById(id) {
-      let index = -1;
-      for (let i = 0; i < this.products.length; i++) {
-        if (this.products[i].id === id) {
-          index = i;
-          break;
-        }
-      }
-      return index;
-    },
-    createId() {
-      let id = '';
-      var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-      for (var i = 0; i < 5; i++) {
-        id += chars.charAt(Math.floor(Math.random() * chars.length));
-      }
-      return id;
-    },
-    exportCSV() {
-      this.$refs.dt.exportCSV();
-    },
-    confirmDeleteSelected() {
-      this.deleteProductsDialog = true;
-    },
-    deleteSelectedProducts() {
-      this.products = this.products.filter(val => !this.selectedProducts.includes(val));
-      this.deleteProductsDialog = false;
-      this.selectedProducts = null;
-      this.$toast.add({ severity: 'success', summary: 'Successful', detail: 'Products Deleted', life: 3000 });
-    },
-    initFilters() {
-      this.filters = {
-        'global': { value: null, matchMode: FilterMatchMode.CONTAINS },
-      }
-    }
   }
 }
 </script>
